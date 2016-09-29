072692fbcbc84e3418457c10df8bb902131a1cb0
Vladimir Antonov <holixus@gmail.com>
2016-09-29 16:48:30 +0300

    100% tests coverage

--- test/md5.js
+++ test/md5.js
@@ -24,7 +24,10 @@ md5_pairs.forEach(function (pair) {
 
 var md5crypt_pairs = [
 	[ [ 'hello', '$1$01234567' ], '$1$01234567$8imesxsCtZWLDOPevHEvu.' ],
-	[ [ 'password', '$1$01234567' ], '$1$01234567$b5lh2mHyD2PdJjFfALlEz1' ]
+	[ [ 'hello', '01234567' ], '$1$01234567$8imesxsCtZWLDOPevHEvu.' ],
+	[ [ 'password', '$1$01234567' ], '$1$01234567$b5lh2mHyD2PdJjFfALlEz1' ],
+	[ [ 'The quick brown fox jumps over the lazy dog', '$1$a' ], '$1$a$bcijBrS0CrSE/jnM0vp.n0' ],
+	[ [ 'The quick brown fox jumps over the lazy dog.', '$1$b' ], '$1$b$TsGXWxAErZ0eD5oJ5UD7a/' ],
 ];
 
 md5crypt_pairs.forEach(function (pair) {
@@ -35,3 +38,31 @@ md5crypt_pairs.forEach(function (pair) {
 		t.end();
 	});
 });
+
+for (var i = 0, n = 9; i < n; ++i)
+	(function (i) {
+		test('salt('+i+' chars)', function (t) {
+			var s = md5.salt(i);
+			t.plan(2);
+			t.equal(s.length, (i ? i : 8), 'check salt length');
+			t.ok(/^[a-zA-Z0-9\.\/]+$/.test(s), 'check salt characters');
+			t.end();
+		});
+	})(i);
+
+test("md5.crypt(too long key)", function (t) {
+	t.plan(1);
+	t.throws(function () {
+		md5.crypt(md5.salt(65));
+	}, /too long key/);
+	t.end();
+});
+
+test("md5.crypt(without salt)", function (t) {
+	t.plan(1);
+	var key = 'blah',
+	    hash = md5.crypt(key);
+	t.equal(hash, md5.crypt(key, hash));
+	t.end();
+});
+
